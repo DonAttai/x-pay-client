b31a92d2c6bf76ff3cff4632b5e7a5831c2214ef
John Attai Yinusa <donattai1032@gmail.com>
2024-05-26 16:44:38 +0100

    modify transfer-money-dialog.tsx

--- src/components/ui/wallet/transfer-money-dialog.tsx
+++ src/components/ui/wallet/transfer-money-dialog.tsx
@@ -1,6 +1,15 @@
 import { Dialog, DialogContent, DialogFooter } from "@/components/ui/dialog";
 import { Button } from "../button";
 import { FormDataType } from "./transfer-money";
+import { useQuery } from "@tanstack/react-query";
+import axiosInstance from "@/hooks/axios";
+
+function formatNaira(amount: number) {
+  return new Intl.NumberFormat("en-NG", {
+    style: "currency",
+    currency: "NGN",
+  }).format(amount);
+}
 
 type PropType = {
   isModalOpen: boolean;
@@ -17,23 +26,44 @@ export const TransferMoneyDialog = ({
   handleSubmit,
   isPending,
 }: PropType) => {
+  // get beneficiary wallet
+
+  const { data: beneficiaryWallet, isSuccess } = useQuery({
+    queryKey: ["beneficiaryWallet", data.walletId],
+    queryFn: async () => {
+      const res = await axiosInstance.get(`users/wallets/${data.walletId}`);
+      return res.data;
+    },
+    enabled: !!data.walletId,
+  });
+
+  // return null when modal is closed
   if (!isModalOpen) return null;
+
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
       <DialogContent>
-        Transfer {data.amount} to {data.walletId}
+        {beneficiaryWallet
+          ? `Transfer ${formatNaira(+data.amount)} to
+          ${beneficiaryWallet?.user.firstName} ${
+              beneficiaryWallet?.user.lastName
+            }`
+          : `${data.walletId} is invalid walletId`}
+
         <DialogFooter>
           <Button variant="secondary" onClick={onClose}>
             Cancel
           </Button>
-          <Button
-            type="button"
-            variant="outline"
-            disabled={isPending}
-            onClick={() => handleSubmit(data)}
-          >
-            {isPending ? "Loading..." : "Confirm"}
-          </Button>
+          {beneficiaryWallet && (
+            <Button
+              type="button"
+              variant="outline"
+              disabled={isPending}
+              onClick={() => handleSubmit(data)}
+            >
+              {isPending ? "Loading..." : "Confirm"}
+            </Button>
+          )}
         </DialogFooter>
       </DialogContent>
     </Dialog>
