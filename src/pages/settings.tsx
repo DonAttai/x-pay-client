c737ccb39295e5ac2a52b1b396ff57a96467ee22
John Attai Yinusa <donattai1032@gmail.com>
2024-05-26 14:37:36 +0100

    modify the ui

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,8 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { useUser } from "@/hooks/use-user";
 
 export const Settings = () => {
-  const { data: user } = useUser();
   return (
     <div className="space-x-4">
       <Button variant={"outline"}>Enable 2FA</Button>
