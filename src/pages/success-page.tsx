051e1427abde01770ad110f1a40b9cc60719cfe2
John Attai Yinusa <donattai1032@gmail.com>
2024-05-27 12:16:20 +0100

    add reference to success page

--- src/pages/success-page.tsx
+++ src/pages/success-page.tsx
@@ -1,10 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 
 export const SuccessPage = () => {
+  const [searchParams] = useSearchParams();
+  const reference = searchParams.get("reference");
   return (
     <div className="h-screen flex flex-col items-center justify-center  space-y-4">
       <p className="text-blue-400 text-4xl">X-PAY</p>
+      {reference ? <p>Reference: {reference}</p> : null}
       <p>Congratulations! You have successfully fund your wallet.</p>
       <Link to="/dashboard/wallet">
         <Button variant="secondary">Back To Wallet</Button>
